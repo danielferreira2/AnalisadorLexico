5170dcee247139caf81a4ec9851be186c94119ab
Daniel de Faria Ferreira <46633039+danielferreira2@users.noreply.github.com>
2022-10-28 10:55:56 -0300

    .

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,10 +4,11 @@ fun main() {
 
   val file = File("src/main/resources/test.txt")
   val input = file.readText()
+
   val lexScanner = LexScanner(input)
+  println(lexScanner.tokenTable)
+
   val outFile = File("src/main/resources/output.txt")
-
-
   outFile.writeText(lexScanner.tokenTable.toString())
 
 }
